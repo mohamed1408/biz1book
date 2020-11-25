ee8c6b9772efe905873d4db4b719d02d39042ebd
Karthick <karthick.nath@gmail.com>
2020-11-25 10:22:19 +0530

    sddf

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -12,13 +12,13 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   getissues() {
-    return this.http.get(this.base_url1 + "Issue/GetIssues");
+    return this.http.get(this.base_url + "Issue/GetIssues");
   }
   GetCompany() {
-    return this.http.get(this.base_url1 + "Company/GetAll");
+    return this.http.get(this.base_url + "Company/GetAll");
   }
   GetUPCompanies() {
-    return this.http.get(this.base_url1 + "Company/GetUPCompanies")
+    return this.http.get(this.base_url + "Company/GetUPCompanies")
   }
 
 
@@ -29,5 +29,21 @@ export class AuthService {
         'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4NSwidXNlcm5hbWUiOiJrYXJ0aGlrK2JpejFib29rIiwiZXhwIjoxNjA2ODQ2MTA5LCJlbWFpbCI6ImFkbWluQGJpejFib29rLmNvbSIsIm9yaWdfaWF0IjoxNjA2MjQxMzA5LCJpc19zdGFmZiI6ZmFsc2UsImZpcnN0X25hbWUiOiJLYXJ0aGlrIiwibGFzdF9uYW1lIjoiIiwicGVybWlzc2lvbnMiOlsiY2FuX2NyZWF0ZV9waXBlbGluZSIsImNhbl91cGRhdGVfZmVhdHVyZV9zdGF0ZSIsImNhbl92aWV3X2ludm9pY2UiXSwicGFydG5lcl9zdGF0ZXMiOlt7ImlkIjoxLCJuYW1lIjoibmV3IiwiY29sb3IiOiJibHVlIn0seyJpZCI6MiwibmFtZSI6InVuZGVyLWRldiIsImNvbG9yIjoiY3lhbiJ9LHsiaWQiOjMsIm5hbWUiOiJvbi1ob2xkIiwiY29sb3IiOiJvcmFuZ2UifSx7ImlkIjo0LCJuYW1lIjoiYWN0aXZlIiwiY29sb3IiOiJncmVlbiJ9LHsiaWQiOjUsIm5hbWUiOiJjaHVybmVkIiwiY29sb3IiOiJyZWQifV0sImZlYXR1cmVfc3RhdGVzIjpbeyJpZCI6MSwibmFtZSI6Im5ldyIsImNvbG9yIjoiYmx1ZSJ9LHsiaWQiOjIsIm5hbWUiOiJpbi1wcm9ncmVzcyIsImNvbG9yIjoiY3lhbiJ9LHsiaWQiOjMsIm5hbWUiOiJ0by1iZS12ZXJpZmllZCIsImNvbG9yIjoib3JhbmdlIn0seyJpZCI6NCwibmFtZSI6ImNvbXBsZXRlZCIsImNvbG9yIjoiZ3JlZW4ifV0sIm1lcmNoYW50X3N0YXRlcyI6W3siaWQiOjEsIm5hbWUiOiJuZXciLCJjb2xvciI6ImJsdWUifSx7ImlkIjoyLCJuYW1lIjoiYWN0aXZlIiwiY29sb3IiOiJncmVlbiJ9LHsiaWQiOjMsIm5hbWUiOiJjaHVybmVkIiwiY29sb3IiOiJyZWQifSx7ImlkIjo0LCJuYW1lIjoibWlncmF0ZWQiLCJjb2xvciI6IiJ9XSwicGxhdGZvcm1zIjpbInpvbWF0byIsInN3aWdneSIsImR1bnpvIiwidWJlcmVhdHMiLCJ6b21hdG9tYXJrZXQiLCJzd2lnZ3lzdG9yZSIsImRvdHBlIl0sInBvc19wbGF0Zm9ybXMiOlsiZGVza3RvcCIsIndlYiIsImFwcCJdLCJxdWlja2Jvb2tzX2F1dGgiOmZhbHNlLCJjdXJyZW5jeSI6IklOUiJ9.1PSZ7iw3OE_ZEDuEIgM1T-sSYq4f4RMmunAj73bYPG8'
       })
     });
+  }
+  getmerchant(bizid) {
+    return this.http.get(this.base_url + "Company/getbybizid?bizid=" + bizid);
+  }
+  getwebhooks(apikey) {
+    return this.http.get("https://api.urbanpiper.com/external/api/v1/webhooks/", {
+      headers: new HttpHeaders({
+        'Authorization': "apikey "+apikey
+      })
+    })
+  }
+  addwebhook() {
+
+  }
+  updatewebhook() {
+
   }
 }
--- src/app/pages/urbanpiper/urbanpiper.component.ts
+++ src/app/pages/urbanpiper/urbanpiper.component.ts
@@ -16,7 +16,20 @@ export class UrbanpiperComponent implements OnInit {
   hide = true;
   editprofile = true;
   url: string | ArrayBuffer;
+
   merchants: any = [];
+  current_merchant = { name: '', username: '', apikey: '', webhooks: [] };
+  showdetails: boolean = false;
+  gotdata: boolean = false;
+  webhooks = [
+    { name: "store_creation", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/AddStoreCallback" },
+    { name: "inventory_update", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/CatalogueCallback" },
+    { name: "order_placed", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/Order" },
+    { name: "item_state_toggle", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/ItemActionCallback" },
+    { name: "order_status_update", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/OrderStatusCallBack" },
+    { name: "rider_status_update", url: "https://biz1pos.azurewebsites.net/api/UrbanPiper/RiderStatusCallback" }
+  ]
+  unconfiguredwebhooks = [];
   constructor(
     private dialog: MatDialog,
     private auth: AuthService
@@ -38,6 +51,29 @@ export class UrbanpiperComponent implements OnInit {
       console.log(data);
       this.merchants = data["results"];
     })
+  }
+  getmerchant(bizid, merchant_name) {
+    this.showdetails = true;
+    this.auth.getmerchant(bizid).subscribe(data => {
+      console.log(data);
+      this.current_merchant.name = merchant_name;
+      this.current_merchant.username = data["UPUsername"];
+      this.current_merchant.apikey = data["UPAPIKey"];
+
+      this.gotdata = true;
+    })
+  }
+  getwebhooks() {
+    if (this.current_merchant.username != '' && this.current_merchant.apikey != '') {
+      this.auth.getwebhooks(this.current_merchant.username + ':' + this.current_merchant.apikey).subscribe(data => {
+        console.log(data)
+        this.unconfiguredwebhooks = this.webhooks;
+        data["webhooks"].forEach(hook => {
+          this.unconfiguredwebhooks = this.unconfiguredwebhooks.filter(x => x.name != hook.event_type);
+        });
+        this.current_merchant.webhooks = data["webhooks"];
+      })
+    }
   }
   getuptable() {
     this.auth.GetUPCompanies().subscribe(data => {
